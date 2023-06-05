10ebf22db90270474513e44a5478044036345a95
ryan echternacht <ryanechternacht@ryans-MacBook-Pro.local>
2023-06-05 19:40:30 -0400

    Add item editing

--- components/Editable/EditableList.vue
+++ components/Editable/EditableList.vue
@@ -1,23 +1,37 @@
 <template>
   <div>
     <ul v-if="edit" class="edit">
-      <li v-for="(item, index) in innerItems">
-        <textarea 
-          class="partner-content"
-          :value="innerItems[index]"
-          @blur="inputChanged($event.target.value, index)" />
+      <li v-for="(_, index) in innerItems">
+        <div class="flex flex-row items-center">
+          <!-- I'd love to make these autogrow -->
+          <textarea 
+            :value="innerItems[index]"
+            @blur="inputChanged($event.target.value, index)" />
+          <div class="w-[40px] flex flex-col items-center">
+            <PButton @click="removeIndex(index)">X</PButton>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="flex flex-row items-center">
+          <textarea
+            v-model="newItem"
+            placeholder="Add a new Row"
+            @blur="addNewItem" />
+          <div class="w-[40px]" />
+        </div>
       </li>
     </ul>
     <ul v-else class="view">
       <li v-for="item in items">
           <span class="partner-content">{{ item }}</span>
-        </li>
+      </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import { clone } from 'lodash';
+import { clone, remove } from 'lodash';
 
 const props = defineProps({
   items: Array,
@@ -43,16 +57,33 @@ function inputChanged (newValue, index) {
   newObject[index] = newValue
   emit('input', newObject)
 }
+
+function removeIndex(i) {
+  // innerItems.value = [...innerItems.value.slice(0, i), ...innerItems.value.slice(i + 1)]
+  emit('input', [...innerItems.value.slice(0, i), ...innerItems.value.slice(i + 1)])
+}
+
+const newItem = ref('')
+function addNewItem() {
+  if (newItem.value) {
+    innerItems.value = [...innerItems.value, newItem.value]
+    newItem.value = ''
+    console.log(innerItems.value)
+    emit('input', innerItems.value)
+  }
+}
 </script>
 
 <style lang="postcss" scoped>
 ul.edit {
   @apply list-disc list-outside ml-4;
+
+  textarea {
+    @apply w-full text-sm border-b-[1.5px] border-gray-300  
+      align-text-top text-gray-700 mb-1 w-full;
+  }
 }
 
-textarea.partner-content {
-  @apply w-full text-sm border border-gray-600 rounded-sm p-[1px] align-text-top;
-}
 
 ul.view {
   @apply list-disc list-outside ml-4;
