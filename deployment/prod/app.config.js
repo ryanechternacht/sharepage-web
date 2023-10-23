7266fdf166e2710f12b73c44282900c332650d3e
ryan echternacht <ryanechternacht@ryans-MacBook-Pro-2.local>
2023-10-23 00:42:30 -0400

    Switch out to use prod heap key for prod

--- deployment/prod/app.config.js
+++ deployment/prod/app.config.js
@@ -2,6 +2,9 @@ export default defineAppConfig({
   stytch: {
     baseUrl: "https://api.stytch.com",
     publicToken: "public-token-live-8c2b086c-f622-4dbe-8a04-5b623bad9300",
-    redirectUri: "https://app.api.thebuyersphere.com/v0.1/login"
-  }
+    redirectUri: "https://app.api.thebuyersphere.com/v0.1/login",
+  },
+  heap: {
+    id: 3969496045,
+  },
 })
