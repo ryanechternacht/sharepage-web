764b198c5187cfa75a07e3d72ae36c4b61ee84d7
ryan echternacht <ryanechternacht@ryans-MacBook-Pro-2.local>
2023-10-23 23:28:52 -0400

    Add heap code to identify users and add additional props

--- app.vue
+++ app.vue
@@ -10,15 +10,24 @@
 
 <script setup>
 import { ModalsContainer } from 'vue-final-modal'
+import { useUsersStore  } from '@/stores/users'
+import { storeToRefs } from 'pinia'
 
-const { heap } = useAppConfig()
+const usersStore = useUsersStore()
+const { getMeCached } = storeToRefs(usersStore)
 
-useHead({
-  script: [{ innerHTML: 
-    `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-    heap.load("${heap.id}");`
-    }]
-})
+const user = await getMeCached.value()
+
+const { identify } = useHeap()
+
+if (process.client && user) {
+  identify(user.id, {
+    name: `${user.firstName} ${user.lastName}`,
+    email: user.email,
+    organizationId: user.organizationId,
+    role: user.buyersphereRole    
+  })
+}
 </script>
 
 <style lang="postcss">
---
+++ composables/useHeap.js
@@ -0,0 +1,39 @@
+// This composable only returns a value when called from the client side
+
+let heapLoading = false
+
+export const useHeap = () => {
+  if (!heapLoading) {
+    heapLoading = true
+    const { heap } = useAppConfig()
+    useHead({
+      script: [{ innerHTML: 
+        `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+        heap.load("${heap.id}");`
+        }]
+    })
+  }
+
+  // calls heap.identify(id) and heap.addUserProperties(props)
+  function identify(id, props) {
+    if (!process.client) {
+      return;
+    }
+
+    if (window?.heap?.loaded) {
+      window.heap.identify(id)
+      heap.addUserProperties(props)
+    } else {
+      setTimeout(() => {
+        identify(id, props)
+      }, 1000)
+    }
+  }
+
+  // TODO Add any other heap calls here, using the timeout form to ensure
+  // heap is loaded and expose below
+
+  return {
+    identify,
+  }
+}
