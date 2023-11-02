c304973e5680ab4f767afa53229ffce33c15b2bd
ryan echternacht <ryanechternacht@ryans-MacBook-Pro-2.local>
2023-11-01 21:41:41 -0400

    Add in a favicon

--- app.vue
+++ app.vue
@@ -28,6 +28,14 @@ if (process.client && user) {
     role: user.buyersphereRole    
   })
 }
+
+useHead({ 
+  link: [{
+    rel: "icon",
+    type: "image/png",
+    href: "https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2F6fe24110aa9d6320a1c004e55c941875.cdn.bubble.io%2Ff1697223014516x964641720147776600%2FFrame%25202.png?w=128&h=&auto=compress&dpr=1&fit=max",
+  }]
+})
 </script>
 
 <style lang="postcss">
