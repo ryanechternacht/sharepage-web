d81937f95d32f5e90d023d986713a6c6995e6382
ryan echternacht <ryan@echternacht.org>
2024-01-11 21:41:47 -0500

    add styling for new insight types

--- components/Buyersphere/BuyersphereInsightsSection.vue
+++ components/Buyersphere/BuyersphereInsightsSection.vue
@@ -9,9 +9,10 @@
         class="item-list-row">
         <img :src="activityMap[item.activity]?.icon"
           class="w-[1rem] h-[1rem]">
-        <div>{{ activityMap[item.activity]?.label }}</div>
-        
-        <div class="flex-grow" />
+        <Tag2 color="gray">{{ activityMap[item.activity]?.label }}</Tag2>
+        <div 
+        v-html="activityMap[item.activity]?.detailsFn(item.activityData)"
+        class="main-content inline-html" />
 
         <UserAvatar :user="item.user" />
         <div class="ml-[-.5rem] min-w-[8rem]">{{ item.user.firstName}} {{ item.user.lastName }}</div>
@@ -21,7 +22,7 @@
           <div class="ml-[-.5rem] min-w-[8rem]">{{ item.buyersphere.buyer }}</div>
         </template>
 
-        <div class="tag min-w-[3rem]">
+        <div class="tag min-w-[4rem]">
           {{ prettyFormatDateFromToday(item.createdAt) }}
         </div>
       </div>
@@ -56,7 +57,91 @@ function prettyFormatDateFromToday(date) {
 }
 
 const activityMap = {
-  'site-activity': { icon: '/svg/eye.svg', label: 'Opened the Buyersphere' }
+  'site-activity': {
+    icon: '/svg/eye.svg',
+    label: 'Opened Buyersphere',
+    detailsFn: () => '',
+  },
+  'create-activity': {
+    icon: '/svg/eye.svg',
+    label: 'Created Activity',
+    detailsFn: (activityData) => activityData?.message,
+  },
+  'edit-activity': {
+    icon: '/svg/eye.svg',
+    label: 'Edited Activity',
+    detailsFn: (activityData) => activityData?.message,
+  },
+  'resolve-activity': {
+    icon: '/svg/eye.svg',
+    label: 'Resolved Activity',
+    detailsFn: (activityData) => activityData?.message,
+  },
+  'unresolve-activity': {
+    icon: '/svg/eye.svg',
+    label: 'Re-opened Activity',
+    detailsFn: (activityData) => activityData?.message,
+  },
+  'delete-activity': {
+    icon: '/svg/eye.svg',
+    label: 'Deleted Activity',
+    detailsFn: (activityData) => activityData?.message,
+  },
+  'edit-constraints': {
+    icon: '/svg/eye.svg',
+    label: '',
+    detailsFn: () => 'Edited the Deal Constraints'
+  },
+  'edit-success-criteria': {
+    icon: '/svg/eye.svg',
+    label: 'Edited the Deal Success Criteria',
+    detailsFn: () => '',
+  },
+  'edit-features': {
+    icon: '/svg/eye.svg',
+    label: 'Edited the Deal Features',
+    detailsFn: () => '',
+  },
+  'edit-objectives': { 
+    icon: '/svg/eye.svg', 
+    label: 'Edited the Deal Objectives',
+    detailsFn: () => '',
+  },
+  'open-asset': {
+    icon: '/svg/eye.svg',
+    label: 'Opened Asset',
+    detailsFn: (activityData) => activityData?.title,
+  },
+  'click-share': { 
+    icon: '/svg/eye.svg', 
+    label: 'Clicked the Share Button',
+    detailsFn: () => '',
+  },
+  'hold-deal': {
+    icon: '/svg/eye.svg',
+    label: 'Put the Deal on Hold',
+    detailsFn: () => '',
+  },
+  'reactivate-deal': { 
+    icon: '/svg/eye.svg', 
+    label: 'Reactivated the Deal',
+    detailsFn: () => '',
+  },
+  'invite-user': {
+    icon: '/svg/eye.svg',
+    label: 'Invited User',
+    detailsFn: (activityData) => activityData?.user,
+  },
+  'edit-user': {
+    icon: '/svg/eye.svg',
+    label: 'Edited User',
+    detailsFn: (activityData) => activityData?.user,
+  },
+  'remove-user': {
+    icon: '/svg/eye.svg',
+    label: 'Removed User',
+    detailsFn: (activityData) => activityData?.user,
+  },
 }
 </script>
 
@@ -67,6 +152,14 @@ const activityMap = {
   &:hover {
     @apply bg-gray-hover px-4 mx-[-1rem] py-2 my-[-.5rem];
     width: calc(100% + 2rem);
+  }
+
+  * {
+    @apply shrink-0;
+  }
+
+  .main-content {
+    @apply shrink grow;
   }
 }
 </style>
