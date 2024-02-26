f78fc58dbb729ffe27f5a27616bfee7b40ddd9d4
ryan echternacht <ryan@echternacht.org>
2024-02-26 09:55:36 -0500

    implement underling for page templates on the settings page too (like for pages)
    Also change Assets Templates to Pre-set Assets

--- pages/settings/[[section]]/[[id]].vue
+++ pages/settings/[[section]]/[[id]].vue
@@ -3,31 +3,32 @@
     <TopNav />
     <div class="page-layout">
       <div class="[grid-area:top] page-top bg-gray-hover">
-        <h1>{{ startCase(mainSection) }} Settings</h1>
+        <h1>{{ header }} Settings</h1>
       </div>
 
       <div class="[grid-area:left]">
         <div class="left-sidebar">
           <h3 class="mb-4">Settings</h3>
+          <!-- TODO use router-link-active instead of manually reimplmenting -->
           <NuxtLink class="page-link"
             :to="`/settings/personal`"
-            :class="{underline: mainSection === 'personal'}">Personal</NuxtLink>
+            :class="{underline: currentPage === 'personal'}">Personal</NuxtLink>
           <NuxtLink class="page-link"
             :to="`/settings/timeline-template`"
-            :class="{underline: mainSection === 'timeline-template'}">Timeline Template</NuxtLink>
+            :class="{underline: currentPage === 'timeline-template'}">Timeline Template</NuxtLink>
           <NuxtLink class="page-link"
-            :to="`/settings/assets-template`"
-            :class="{underline: mainSection === 'assets-template'}">Assets Template</NuxtLink>
+            :to="`/settings/preset-assets`"
+            :class="{underline: currentPage === 'preset-assets'}">Pre-set Assets</NuxtLink>
           <NuxtLink class="page-link"
             :to="`/settings/admin`"
-            :class="{underline: mainSection === 'admin'}">Admin</NuxtLink>
+            :class="{underline: currentPage === 'admin'}">Admin</NuxtLink>
 
           <div class="h-[80px]" />
 
           <h3 class="mb-4">Page Templates</h3>
           <NuxtLink v-for="pt in pageTemplates"
             class="page-link"
-            :class="{underline: pt.id === mainSection}"
+            :class="{underline: `${pt.id}` === currentPage}"
             :to="`/settings/page-template/${pt.id}`">
             {{ pt.title }}
           </NuxtLink>
@@ -36,11 +37,11 @@
       </div>
 
       <!-- These return the left, center, left-header, and center-header sections -->
-      <SettingsPersonal v-if="mainSection === 'personal'" />
-      <SettingsAdmin v-else-if="mainSection === 'admin'" />
-      <!-- <SettingsGuideTemplate v-else-if="mainSection === 'guide-template'" /> -->
-      <SettingsTimelineTemplate v-else-if="mainSection === 'timeline-template'" />
-      <SettingsAssetsTemplate v-else-if="mainSection === 'assets-template'" />
+      <SettingsPersonal v-if="currentPage === 'personal'" />
+      <SettingsAdmin v-else-if="currentPage === 'admin'" />
+      <!-- <SettingsGuideTemplate v-else-if="currentPage === 'guide-template'" /> -->
+      <SettingsTimelineTemplate v-else-if="currentPage === 'timeline-template'" />
+      <SettingsAssetsTemplate v-else-if="currentPage === 'preset-assets'" />
       <SettingsPageTemplate v-else />
 
       <div class="[grid-area:footer] h-20" />
@@ -64,15 +65,26 @@ const [pageTemplates] = await Promise.all([
 ])
 
 const route = useRoute()
-const mainSection = computed(() => {
+const currentPage = computed(() => {
   if (!route.params.section) {
     return 'personal'
   } else if (route.params.section === 'page-template') {
-    return find(pageTemplates, pt => pt.id === parseInt(route.params.id)).title
+    return route.params.id
   } else {
     return route.params.section
   }
 })
+
+const header = computed(() => {
+  if (route.params.section === 'page-template') {
+    return find(pageTemplates, pt => pt.id === parseInt(route.params.id)).title
+  } else if (route.params.section === 'preset-assets') {
+    return 'Pre-set Assets'
+  } else {
+    return startCase(currentPage.value)
+  }
+})
+
 const router = useRouter()
 const { open, close } = useModal({
   component: AddBuyerspherePageModal,
