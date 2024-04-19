423de492e622820392289310adb3f8bd9b32dd68
ryan echternacht <ryan@echternacht.org>
2024-04-19 10:59:39 -0400

    first bad draft

---
+++ layouts/swaypage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <TopNav />
+    <div class="page-grid">
+      <div class="flex flex-col gap-2">
+        <div>Pied Piper</div>
+        <div>Feed</div>
+        <div>Activities</div>
+        <div>Page 1</div>
+        <div>Page 2</div>
+        <div>Page 3</div>
+      </div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="postcss" scoped>
+.page-grid {
+  @apply mx-8 grid;
+  /* TODO, I can probably improve these a bit using some minmax() */
+  /* ideal is ~350 710 220 */
+  grid-template-columns: 3fr 6.5fr 2fr;
+
+  :deep() > :nth-child(2) {
+    @apply border border-gray-border rounded-md px-2 py-1;
+    /* TODO this is based on the current top nav height */
+    min-height: calc(100vh - 5rem);
+  }
+}
+</style>
---
+++ pages/swaypage-demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+   I'm real page content!
+  </div>
+  <div class="mt-40 flex flex-col gap-4 items-end">
+    <div class="gray">Key Links</div>
+    <div>Schedule a Call</div>
+    <div>Connect on LinkedIn</div>
+    <div>See our Testemonials</div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'swaypage'
+})
+</script>
+
+<style lang="postcss" scoped>
+</style>
