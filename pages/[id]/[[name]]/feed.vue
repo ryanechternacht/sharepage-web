ba19f0e2568579c9c4ba243fbdba2ca4e660eb40
ryan echternacht <ryan@echternacht.org>
2024-04-30 12:52:10 -0400

    first pass at a fee dpage

--- pages/[id]/[[name]]/feed.vue
+++ pages/[id]/[[name]]/feed.vue
@@ -31,13 +31,26 @@
       </dropdown-menu>
     </div>
     <div class="page-area">
-      feed
+      <div class="feed-grid">
+        <h2>Lead</h2>
+        <h2>Event</h2>
+        <h2>Event Time</h2>
+
+        <template v-for="session in buyerSessions">
+          <div>{{ session?.linkedName }}</div>
+          <div>{{ reformatView(session) }}</div>
+          <div>{{ session.createdAt }}</div>
+        </template>
+      </div>
     </div>
   </div>     
 </template>
 
 <script setup>
-
+import { useSwaypagesStore } from '@/stores/swaypages'
+import { storeToRefs } from 'pinia'
+import lodash_pkg from 'lodash';
+const { concat, filter, join, map, orderBy } = lodash_pkg;
 
 definePageMeta({
   name: 'feed',
@@ -45,7 +58,24 @@ definePageMeta({
   // TODO middleware
 })
 
+const route = useRoute()
+const swaypageId = parseInt(route.params.id)
 
+const swaypagesStore = useSwaypagesStore()
+const {
+  getSwaypageBuyerSessionsByIdCached,
+} = storeToRefs(swaypagesStore)
+
+const [buyerSessions] = await Promise.all([
+  getSwaypageBuyerSessionsByIdCached.value(swaypageId),
+])
+
+function reformatView (item) {
+  return join(
+    map(item.timings, i => `${i.title}: ${i.timeOnPage} seconds`),
+    ' | '
+  )
+}
 </script>
 
 <style lang="postcss" scoped>
@@ -53,6 +83,21 @@ definePageMeta({
   @apply border border-gray-border-dark rounded-md px-2 py-1;
   /* TODO this is based on the current top nav height */
   min-height: calc(100vh - 5rem);
+  overflow: hidden;
 }
 
+.feed-grid {
+  @apply grid px-8 gap-x-8;
+  grid-template-columns: auto 1fr auto;
+  grid-auto-rows: 3rem;
+
+  div {
+    @apply relative;
+  
+    &::after {
+      @apply absolute bg-gray-border -start-12 h-[1px] w-screen;
+      content: '';
+    }
+  }
+}
 </style>
