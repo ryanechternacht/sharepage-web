5471df039ec6b07d09254aaee0a92b9acb6a4356
ryan echternacht <ryan@echternacht.org>
2024-06-21 15:19:31 -0400

    rename to confirm working

--- pages/[id]/[[name]]/settings.vue
+++ pages/[id]/[[name]]/settings.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="h-[2.375rem] flex flex-row items-center gap-6">
-      <h1>Feed</h1>
+      <h1>Settings</h1>
       <div class="flex-grow" />
       <!-- <div>active</div> -->
       <div class="flex flex-row items-center gap-2">
