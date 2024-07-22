0cd386f1900cfbbd43311ded30db0a630e67eb4a
ryan echternacht <ryan@echternacht.org>
2024-07-22 15:57:01 -0400

    update dev config

--- deployment/dev/app.config.js
+++ deployment/dev/app.config.js
@@ -6,7 +6,7 @@ export default defineAppConfig({
   stytch: {
     baseUrl: "https://test.stytch.com",
     publicToken: "public-token-test-3d685515-f94a-4444-96f6-fd54e4d4c1e2",
-    redirectUri: "https://app.api.thebuyersphere-staging.com/v0.1/login",
+    redirectUri: "https://app.api.swaypage-dev.io/v0.1/login",
   },
   heap: {
     id: 3326826010,
@@ -15,6 +15,6 @@ export default defineAppConfig({
     salesforce: true,
   },
   cookies: {
-    domain: '.thebuyersphere-staging.com'
+    domain: '.swaypage-dev.io'
   },
 })
