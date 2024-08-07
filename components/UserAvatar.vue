1b6bc0b369a1a5cc1ae25917aa37bc30c1085c84
ryan echternacht <ryan@echternacht.org>
2024-08-07 16:54:58 -0400

    Fix fallback image gettings squished

--- components/UserAvatar.vue
+++ components/UserAvatar.vue
@@ -3,12 +3,12 @@
     :class="[ sizeClasses ]">
     <UIcon class="w-auto h-auto" name="i-heroicons-user" />
   </div>
-  <img v-else-if="user?.image"
+  <img v-else-if="false"
     :class="[ sizeClasses ]"
     class="rounded-full"
     :src="user?.image">
   <div v-else
-    :class="[ sizeClasses ]"
+    :class="[ sizeClassesForFallback ]"
     class="bg-blue-500 rounded-full center-xy h-8">
     <h3 class="dark mx-auto">{{ user?.firstName[0] }}</h3>
   </div>
@@ -25,6 +25,12 @@ const sizeClasses = computed(() => ({
   'small': 'max-w-[1rem] max-h-[1rem] w-full',
   'medium': 'max-w-[1.5rem] max-h-6 w-full',
   'large': 'max-w-[2rem] max-h-8 w-full',
+}[props.size ?? 'medium']))
+
+const sizeClassesForFallback = computed(() => ({
+  'small': 'w-[1rem] h-[1rem]',
+  'medium': 'w-[1.5rem] h-[1.5rem]',
+  'large': 'w-[2rem] h-[2rem]',
 }[props.size ?? 'medium']))
 </script>
 
