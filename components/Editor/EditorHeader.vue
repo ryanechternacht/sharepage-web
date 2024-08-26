1fc41a8a00a551c15661c00dbbeae0aba9ec52f3
ryan echternacht <ryan@echternacht.org>
2024-08-26 17:31:46 -0400

    Multiline headers

--- components/Editor/EditorHeader.vue
+++ components/Editor/EditorHeader.vue
@@ -8,13 +8,14 @@
       <div v-if="readonly"
         class="text-lg font-bold text-black w-full"
         v-html="value" />
-      <input v-else 
-        v-model="value"
-        class="p-0 border-0 rounded-none text-lg font-bold text-black w-full"
-        :class="{ 'hide-placeholder': readonly }"
+      <textarea v-model="value"
+        rows="1"
+        ref="textarea"
+        class="p-0 border-0 rounded-none text-lg font-bold text-black w-full resize-none"
         placeholder="Header"
         :readonly="readonly"
-        @blur="emit('update:modelValue', value)">
+        @blur="emit('update:modelValue', value)"
+        @input="removeNewLines" />
     </template>
   </EditorItemTemplate>
 </template>
@@ -23,7 +24,7 @@
 import lodash_pkg from 'lodash';
 const { clone } = lodash_pkg;
 
-const props = defineProps({ 
+const props = defineProps({
   modelValue: { type: String },
   readonly: { type: Boolean, default: false },
   includeAiPromptTemplate: { type: Boolean, default: false },
@@ -32,11 +33,39 @@ const props = defineProps({
 
 const emit = defineEmits(['update:modelValue', 'delete:item'])
 
+const textarea = ref(null)
+
 const value = ref(clone(props.modelValue))
+
+// modified from from https://github.com/nuxt/ui/blob/dev/src/runtime/components/forms/Textarea.vue
+// i couldn't use the element directly because adjusting the model value to
+// remove new lines wasn't being propogated back into the component for some reason
+const autoResize = () => {
+  if (!textarea.value) {
+    return
+  }
+
+  textarea.value.rows = 1
+  
+  const styles = window.getComputedStyle(textarea.value)
+  const paddingTop = parseInt(styles.paddingTop)
+  const paddingBottom = parseInt(styles.paddingBottom)
+  const padding = paddingTop + paddingBottom
+  const lineHeight = parseInt(styles.lineHeight)
+  const { scrollHeight } = textarea.value
+  const newRows = (scrollHeight - padding) / lineHeight
+
+  textarea.value.rows = newRows
+}
 
 watch(() => props.modelValue, (newModelValue) => {
   value.value = newModelValue
 })
+
+function removeNewLines() {
+  value.value = value.value.replace(/[\n\r]/g, '')
+  nextTick(autoResize)
+}
 </script>
 
 <style lang="postcss" scoped>
